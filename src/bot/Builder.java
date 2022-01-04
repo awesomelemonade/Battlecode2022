c0748e0dc25c9566ee8d6b45a844c07a121a8746
Lawrence Chen <ldorianchen@gmail.com>
2022-01-04 15:52:33 -0500

    watchtower attack

--- src/bot/Builder.java
+++ src/bot/Builder.java
@@ -1,10 +1,11 @@
 package bot;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotInfo;
+import battlecode.common.*;
+import bot.util.Cache;
 import bot.util.RunnableBot;
 import bot.util.Util;
 
+import static bot.util.Constants.ORDINAL_DIRECTIONS;
 import static bot.util.Constants.rc;
 
 public class Builder implements RunnableBot {
@@ -23,5 +24,27 @@ public class Builder implements RunnableBot {
                 Util.tryKiteFrom(closestEnemyAttacker.location);
             }
         }
+        if (rc.isActionReady()) {
+            RobotInfo closestAllyWatchtower = Util.getClosestRobot(Cache.ALLY_ROBOTS, r -> r.type == RobotType.WATCHTOWER && r.health < r.type.health && rc.canRepair(r.location));
+            if (closestAllyWatchtower != null) {
+                rc.repair(closestAllyWatchtower.location);
+            } else {
+                tryBuild(RobotType.WATCHTOWER);
+            }
+        }
+    }
+
+    boolean tryBuild(RobotType type) throws GameActionException {
+        if (!rc.isActionReady()) return false;
+        for (Direction d: ORDINAL_DIRECTIONS) {
+            MapLocation loc = rc.getLocation().add(d);
+            if ((loc.x + loc.y) % 2 == 0 && loc.x % 2 == 0) {
+                if (rc.canBuildRobot(type, d)) {
+                    rc.buildRobot(type, d);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/bot/Watchtower.java
+++ src/bot/Watchtower.java
@@ -1,7 +1,12 @@
 package bot;
 
 import battlecode.common.GameActionException;
+import battlecode.common.RobotInfo;
+import bot.util.Cache;
+import bot.util.Debug;
 import bot.util.RunnableBot;
+
+import static bot.util.Constants.rc;
 
 public class Watchtower implements RunnableBot {
     @Override
@@ -11,6 +16,20 @@ public class Watchtower implements RunnableBot {
 
     @Override
     public void loop() throws GameActionException {
-
+        if (rc.isActionReady()) {
+            RobotInfo bestRobot = null;
+            int bestHealth = (int)1e9;
+            for (RobotInfo robot : Cache.ENEMY_ROBOTS) {
+                if (rc.canAttack(robot.location)) {
+                    if (robot.health < bestHealth) {
+                        bestHealth = robot.health;
+                        bestRobot = robot;
+                    }
+                }
+            }
+            if (bestRobot != null) {
+                rc.attack(bestRobot.location);
+            }
+        }
     }
 }
