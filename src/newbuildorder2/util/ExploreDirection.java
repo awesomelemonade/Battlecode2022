2122030263f8dcc4e46c7f89fe8bdb373f1fb2de
Jason Lee <awesomelemonade@gmail.com>
2022-01-10 17:05:09 -0500

    Add newbuildorder2

---
+++ src/newbuildorder2/util/ExploreDirection.java
@@ -0,0 +1,61 @@
+package newbuildorder2.util;
+
+import battlecode.common.Direction;
+
+public enum ExploreDirection {
+    N(0, 1),
+    S(0, -1),
+    E(1, 0),
+    W(-1, 0),
+    NW(-1, 1),
+    NE(1, 1),
+    SE(1, -1),
+    SW(-1, -1),
+    NNW(-1, 2),
+    NWW(-2, 1),
+    SWW(-2, -1),
+    SSW(-1, -2),
+    SSE(1, -2),
+    SEE(2, -1),
+    NEE(2, 1),
+    NNE(1, 2);
+
+    public final int dx;
+    public final int dy;
+    public final int sigDx;
+    public final int sigDy;
+
+    private ExploreDirection(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+        this.sigDx = Integer.signum(dx);
+        this.sigDy = Integer.signum(dy);
+    }
+
+    public static boolean isOpposite(ExploreDirection a, ExploreDirection b) {
+        return a.dx == -b.dx && a.dy == -b.dy;
+    }
+
+    public static ExploreDirection fromDirection(Direction direction) {
+        switch (direction) {
+            case NORTH:
+                return N;
+            case SOUTH:
+                return S;
+            case EAST:
+                return E;
+            case WEST:
+                return W;
+            case NORTHEAST:
+                return NE;
+            case NORTHWEST:
+                return NW;
+            case SOUTHEAST:
+                return SE;
+            case SOUTHWEST:
+                return SW;
+            default:
+                throw new IllegalArgumentException("Unknown");
+        }
+    }
+}
---
+++ src/newbuildorder2/util/Profile.java
@@ -0,0 +1,24 @@
+package newbuildorder2.util;
+
+import static newbuildorder2.util.Constants.rc;
+
+public enum Profile {
+    ERROR_STATE(true),
+    CHUNK_INFO(false),
+    PATHFINDER(false),
+    EXPLORER(true),
+    MINING(true),
+    ATTACKING(true);
+
+    private static final int id = -1; // Filter by id
+
+    private final boolean enabled;
+
+    private Profile(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean enabled() {
+        return enabled && (id == -1 || id == rc.getID());
+    }
+}
