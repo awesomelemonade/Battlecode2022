5640942f5020f92bca281a2f3857fc9ea88a99a8
Jason Lee <awesomelemonade@gmail.com>
2022-01-11 00:35:51 -0500

    ls maps

--- run_matches.py
+++ run_matches.py
@@ -25,10 +25,10 @@ numWinsMapping = {
 def run_match(bot, map):
     print("Running {} vs {} on {}".format(currentBot, bot, map))
     try:
-        #outputA = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + currentBot, '-PteamB=' + bot, '-Pmaps="' + map + '"']))
+        outputA = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + currentBot, '-PteamB=' + bot, '-Pmaps="' + map + '"']))
         #outputB = str(subprocess.check_output(['./gradlew', 'run', '-PteamA=' + bot, '-PteamB=' + currentBot, '-Pmaps="' + map + '"']))
-        outputA = ''
-        outputB = str(subprocess.check_output(['ls']))
+        #outputA = ''
+        outputB = str(subprocess.check_output(['ls maps']))
         print("TEST: " + outputB)
     except subprocess.CalledProcessError as exc:
         print("Status: FAIL", exc.returncode, exc.output)
