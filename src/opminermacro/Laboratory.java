ad50573e9ae484947e30bc40f9420f2dc7968c19
Lawrence Chen <ldorianchen@gmail.com>
2022-01-12 18:38:58 -0500

    start comming lead (but nothing is done onChunkChange)

---
+++ src/opminermacro/Laboratory.java
@@ -0,0 +1,24 @@
+package opminermacro;
+
+import battlecode.common.GameActionException;
+import opminermacro.util.RunnableBot;
+
+import static opminermacro.util.Constants.ALLY_TEAM;
+import static opminermacro.util.Constants.rc;
+
+public class Laboratory implements RunnableBot {
+    @Override
+    public void init() throws GameActionException {
+
+    }
+
+    @Override
+    public void loop() throws GameActionException {
+        int lead = rc.getTeamLeadAmount(ALLY_TEAM);
+        if (lead >= 4000 || (rc.getRoundNum() > 1900 && lead > 300) || rc.getRoundNum() > 1950) {
+            if (rc.canTransmute()) {
+                rc.transmute();
+            }
+        }
+    }
+}
---
+++ src/opminermacro/util/Profile.java
@@ -0,0 +1,24 @@
+package opminermacro.util;
+
+import static opminermacro.util.Constants.rc;
+
+public enum Profile {
+    ERROR_STATE(true),
+    CHUNK_INFO(false),
+    PATHFINDER(false),
+    EXPLORER(false),
+    MINING(false),
+    ATTACKING(false);
+
+    private static final int id = -1; // Filter by id
+
+    private final boolean enabled;
+
+    private Profile(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean enabled() {
+        return enabled && (id == -1 || id == rc.getID());
+    }
+}
