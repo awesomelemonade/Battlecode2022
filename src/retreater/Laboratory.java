8675f3112ae8a4e559b9cc657a99dfd0d7bbdacf
Lawrence Chen <ldorianchen@gmail.com>
2022-01-14 17:45:22 -0500

    retreating very op (#10)
    * retreating very op
    
    * fix compile error
    
    * configure run_matches
    
    Co-authored-by: Jason Lee <awesomelemonade@gmail.com>

---
+++ src/retreater/Laboratory.java
@@ -0,0 +1,24 @@
+package retreater;
+
+import battlecode.common.GameActionException;
+import retreater.util.RunnableBot;
+
+import static retreater.util.Constants.ALLY_TEAM;
+import static retreater.util.Constants.rc;
+
+public class Laboratory implements RunnableBot {
+    @Override
+    public void init() throws GameActionException {
+
+    }
+
+    @Override
+    public void loop() throws GameActionException {
+        int lead = rc.getTeamLeadAmount(ALLY_TEAM);
+        if (lead >= 4000 || (rc.getRoundNum() > 1900 && lead > 300) || rc.getRoundNum() > 1950) {
+            if (rc.canTransmute()) {
+                rc.transmute();
+            }
+        }
+    }
+}
