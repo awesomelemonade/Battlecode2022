8fbd458081b0fe6c71d870581352ec27d9a0d17f
Jason Lee <awesomelemonade@users.noreply.github.com>
2022-01-15 00:07:43 -0500

    Passive Retreat (#12)
    * Created passive retreat
    
    * Changed low hp soldiers to passive
    
    * update with opexplore
    
    * configure run_matches for passiveRetreat
    
    Co-authored-by: Kelvin <kelvin.zhang011@gmail.com>

---
+++ src/passiveRetreat/Laboratory.java
@@ -0,0 +1,24 @@
+package passiveRetreat;
+
+import battlecode.common.GameActionException;
+import passiveRetreat.util.RunnableBot;
+
+import static passiveRetreat.util.Constants.ALLY_TEAM;
+import static passiveRetreat.util.Constants.rc;
+
+public class Laboratory implements RunnableBot {
+    @Override
+    public void init() throws GameActionException {
+
+    }
+
+    @Override
+    public void loop() throws GameActionException {
+        int lead = rc.getTeamLeadAmount(ALLY_TEAM);
+        if (lead >= 4000 || (rc.getRoundNum() > 1900 && lead > 300) || rc.getRoundNum() > 1950) {
+            if (rc.canTransmute()) {
+                rc.transmute();
+            }
+        }
+    }
+}
