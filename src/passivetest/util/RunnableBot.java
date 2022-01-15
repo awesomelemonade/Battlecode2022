d7d38c06f679d485849a2575ccf73107c3c969e8
Jason Lee <awesomelemonade@gmail.com>
2022-01-15 17:45:58 -0500

    passivetest

---
+++ src/passivetest/util/RunnableBot.java
@@ -0,0 +1,8 @@
+package passivetest.util;
+
+import battlecode.common.GameActionException;
+
+public interface RunnableBot {
+    public void init() throws GameActionException;
+    public void loop() throws GameActionException;
+}
