64ac6eb8b003cb97041043f0ea2c2c6dde79a8bc
Jason Lee <awesomelemonade@gmail.com>
2022-01-16 18:40:30 -0500

    Delete old packages

--- src/newbuildorder4/util/RunnableBot.java
+++
@@ -1,8 +0,0 @@
-package newbuildorder4.util;
-
-import battlecode.common.GameActionException;
-
-public interface RunnableBot {
-    public void init() throws GameActionException;
-    public void loop() throws GameActionException;
-}
