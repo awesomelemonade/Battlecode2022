060009d6610d780352d666c78329830265d262da
Lawrence Chen <ldorianchen@gmail.com>
2022-01-16 17:54:09 -0500

    Realhealingcomms (#14)
    * passivetest
    
    * go to archon that is not backed up
    
    * remove excess packages
    
    * change archon moving conditions
    
    * change initial miner lower bound to 4
    
    Co-authored-by: Jason Lee <awesomelemonade@gmail.com>

--- src/fixedmicro/util/RunnableBot.java
+++
@@ -1,8 +0,0 @@
-package fixedmicro.util;
-
-import battlecode.common.GameActionException;
-
-public interface RunnableBot {
-    public void init() throws GameActionException;
-    public void loop() throws GameActionException;
-}
