f159977cb1a0c5cb2378a1ee06f489d4bd2fdf8e
Jason Lee <awesomelemonade@gmail.com>
2022-01-22 23:59:46 -0500

    configure run_matches for pogbot

--- run_matches.py
+++ run_matches.py
@@ -10,9 +10,9 @@ emojiMap = {
     'Error': ':heavy_exclamation_mark:'
 }
 errors = []
-currentBot = 'opbot'
+currentBot = 'pogbot'
 
-bots = ['combinedsoldierhealth', 'sages']
+bots = ['combinedsoldierhealth', 'sages', 'opbot']
 botsSet = set(bots)
 # maps = ['maptestsmall', 'eckleburg', 'intersection', 'Barrier', 'BarrierWithLead', 'Circles', 'CloseArchons', 'colosseum', 'fortress', 'jellyfish', 'MoveYourArchons', 'MultiplePortableArchonsMoreLead', 'nottestsmall', 'progress', 'rivers', 'sandwich', 'squer', 'uncomfortable', 'underground', 'valley', 'Z']
 maps = ['maptestsmall', 'eckleburg', 'intersection', 'colosseum', 'fortress', 'jellyfish', 'nottestsmall', 'progress', 'rivers', 'sandwich', 'squer', 'uncomfortable', 'underground', 'valley', 'chessboard', 'collaboration', 'dodgeball', 'equals', 'highway', 'nyancat', 'panda', 'pillars', 'snowflake', 'spine', 'stronghold', 'tower']
