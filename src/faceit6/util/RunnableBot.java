0d4e8330c5ce6772cf226ae0fd30841dc4bd912d
Lawrence Chen <ldorianchen@gmail.com>
2022-01-24 15:23:09 -0500

    initial commit faceit6

---
+++ src/faceit6/util/RunnableBot.java
@@ -0,0 +1,8 @@
+package faceit6.util;
+
+import battlecode.common.GameActionException;
+
+public interface RunnableBot {
+    public void init() throws GameActionException;
+    public void loop() throws GameActionException;
+}
