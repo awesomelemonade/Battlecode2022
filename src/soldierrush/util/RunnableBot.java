0052bb111b46486feaa91ae52565478ec0c5a28f
Jason Lee <awesomelemonade@gmail.com>
2022-01-26 12:03:45 -0500

    Initial Soldier Rush

---
+++ src/soldierrush/util/RunnableBot.java
@@ -0,0 +1,8 @@
+package soldierrush.util;
+
+import battlecode.common.GameActionException;
+
+public interface RunnableBot {
+    public void init() throws GameActionException;
+    public void loop() throws GameActionException;
+}
